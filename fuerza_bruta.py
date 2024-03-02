0ebf9239dcbc65816d5af30831d192f85be05e5b
lizatamara <lizaa1609@gmail.com>
2024-03-01 22:15:55 -0300

    tercer ejercicio

--- fuerza_bruta.py
+++ fuerza_bruta.py
@@ -1,1 +1,16 @@
 from string import ascii_lowercase
+from getpass import getpass
+
+password = getpass("Ingrese la contraseña ").lower()
+
+contador = 0
+
+for i in password:
+    for letra2 in ascii_lowercase:
+        if i == letra2:
+            contador += 1
+            break
+        else:
+            contador += 1
+        
+print(f"{contador} intentos")
